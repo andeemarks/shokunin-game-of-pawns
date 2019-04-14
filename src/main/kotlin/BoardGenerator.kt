b396c96288fb8895b077caddd11f96750409f4e5
Andy Marks <vampwillow@gmail.com>
2019-04-14 21:01:50 +1000

    More cleanup, moving public interface to top of source

--- src/main/kotlin/BoardGenerator.kt
+++ src/main/kotlin/BoardGenerator.kt
@@ -14,9 +14,20 @@ private val EMPTY_SQUARES = EMPTY_SQUARE.toString().repeat(32)
 private const val WHITE_PROMOTION_RANK = 0
 private const val BLACK_PROMOTION_RANK = 7
 
-class BoardGenerator {
+private const val BLACK_KING = 'k'
+private const val WHITE_KING = 'K'
 
+class BoardGenerator {
     var board: List<List<Char>> = populate()
+
+    fun squares() = board.flatten()
+    fun whitePromotionRank(board: List<List<Char>>) = board[WHITE_PROMOTION_RANK]
+
+    fun blackPromotionRank(board: List<List<Char>>) = board[BLACK_PROMOTION_RANK]
+    fun blackKingPosition(board: List<List<Char>>): Pair<Int, Int> = findPositionOfPiece(board, BLACK_KING)
+
+    fun whiteKingPosition(board: List<List<Char>>): Pair<Int, Int> = findPositionOfPiece(board, WHITE_KING)
+    fun areNeighbours(whiteKing: Pair<Int, Int>, blackKing: Pair<Int, Int>): Boolean = (columnDistanceBetween(whiteKing, blackKing) <= 1 && rowDistanceBetween(whiteKing, blackKing) <= 1)
 
     private fun populate(): List<List<Char>> {
         var populatedBoard: MutableList<MutableList<Char>> = "$WHITE_PIECES$BLACK_PIECES$EMPTY_SQUARES".toMutableList().shuffled().chunked(RANK_WIDTH).map { row -> row.toMutableList() }.toMutableList()
@@ -33,7 +44,7 @@ class BoardGenerator {
 
         if (areNeighbours(whiteKingPosition, blackKingPosition)) {
             val newWhiteKingPosition = emptySquares(squares).filter { square -> !areNeighbours(blackKingPosition, square) }.shuffled().first()
-            switchWithEmptySquare(squares, newWhiteKingPosition, whiteKingPosition, 'K')
+            switchWithEmptySquare(squares, newWhiteKingPosition, whiteKingPosition, WHITE_KING)
         }
 
         return squares
@@ -64,23 +75,16 @@ class BoardGenerator {
 
         pawnsToMove.forEachIndexed { i: Int, pawn: Pair<Int, Int> -> switchWithEmptySquare(squares, availableSpots[i], pawn, WHITE_PAWN) }
     }
-
     private fun availableSquaresForWhitePawns(board: List<List<Char>>): List<Pair<Int, Int>> = emptySquares(board).filter { square -> square.first != WHITE_PROMOTION_RANK }
     private fun whitePawnsInPromotionRank(board: List<List<Char>>) = whitePromotionRank(board).mapIndexed { index, square -> if (square == WHITE_PAWN) Pair(WHITE_PROMOTION_RANK, index.rem(RANK_WIDTH)) else null }.filterNotNull()
     private fun blackPawnsInPromotionRank(board: List<List<Char>>) = blackPromotionRank(board).mapIndexed { index, square -> if (square == BLACK_PAWN) Pair(BLACK_PROMOTION_RANK, index.rem(RANK_WIDTH)) else null }.filterNotNull()
+
     private fun availableSquaresForBlackPawns(board: List<List<Char>>) = emptySquares(board).filter { square -> square.first != BLACK_PROMOTION_RANK }
-
     private fun findPositionOfPiece(board: List<List<Char>>, piece: Char) = board.flatten().mapIndexed { i, square -> if (square == piece) Pair(i.div(RANK_WIDTH), i.rem(RANK_WIDTH)) else null }.filterNotNull().first()
+
     private fun emptySquares(board: List<List<Char>>) = board.flatten().mapIndexed { index, square -> if (square == EMPTY_SQUARE) Pair(index.div(RANK_WIDTH), index.rem(RANK_WIDTH)) else null }.filterNotNull()
-
     private fun rowDistanceBetween(whiteKing: Pair<Int, Int>, blackKing: Pair<Int, Int>) = (whiteKing.second - blackKing.second).absoluteValue
-    private fun columnDistanceBetween(whiteKing: Pair<Int, Int>, blackKing: Pair<Int, Int>) = (whiteKing.first - blackKing.first).absoluteValue
 
-    fun squares() = board.flatten()
-    fun whitePromotionRank(board: List<List<Char>>) = board[WHITE_PROMOTION_RANK]
-    fun blackPromotionRank(board: List<List<Char>>) = board[BLACK_PROMOTION_RANK]
-    fun blackKingPosition(board: List<List<Char>>): Pair<Int, Int> = findPositionOfPiece(board, 'k')
-    fun whiteKingPosition(board: List<List<Char>>): Pair<Int, Int> = findPositionOfPiece(board, 'K')
-    fun areNeighbours(whiteKing: Pair<Int, Int>, blackKing: Pair<Int, Int>): Boolean = (columnDistanceBetween(whiteKing, blackKing) <= 1 && rowDistanceBetween(whiteKing, blackKing) <= 1)
+    private fun columnDistanceBetween(whiteKing: Pair<Int, Int>, blackKing: Pair<Int, Int>) = (whiteKing.first - blackKing.first).absoluteValue
 }
 
