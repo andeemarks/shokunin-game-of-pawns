0d96bdf4a89701b85b1b7700e4d71dba89600566
Andy Marks <vampwillow@gmail.com>
2019-04-14 20:42:48 +1000

    After re-import

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,6 +10,7 @@ plugins {
 
     // Apply the application plugin to add support for building a CLI application.
     application
+    kotlin("jvm") version "1.3.30"
 }
 
 repositories {
@@ -20,7 +21,7 @@ repositories {
 
 dependencies {
     // Use the Kotlin JDK 8 standard library.
-    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk10")
 
     // Use the Kotlin test library.
     testImplementation("org.jetbrains.kotlin:kotlin-test")
